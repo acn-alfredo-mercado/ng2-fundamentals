4ead118c75962e2ed2b0b67f51134b803c196f96
Mercado, Alfredo I. R <alfredo.i.r.mercado@accenture.com>
2018-02-05 18:18:20 +0800

    first commit

---
+++ app/events/event-details/event-details.component.ts
@@ -0,0 +1,26 @@
+import { EventService } from './../shared/event.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+    templateUrl: '/app/events/event-details/event-details.component.html',
+    styles: [`
+        .container { padding-left: 20px; padding-right: 20px; }
+        .event-image { height: 100px; }
+    `
+    ]
+})
+
+export class EventDetailsComponent implements OnInit {
+    event: any;
+    
+    constructor(
+        private eventService: EventService,
+        private activatedRoute: ActivatedRoute
+    ) {}
+
+    ngOnInit() {
+        this.event = this.eventService.getEvent(
+            +this.activatedRoute.snapshot.params['id']); 
+    }
+}
