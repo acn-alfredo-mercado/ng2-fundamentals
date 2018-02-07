775de1d5a3bd759ccf76167f74a8691c2b5c9a51
Mercado, Alfredo I. R <alfredo.i.r.mercado@accenture.com>
2018-02-07 15:41:37 +0800

    use @Inject to use Toastr

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,7 +1,8 @@
 import { AuthService } from './auth.service';
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Inject } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Toastr, TOASTR_TOKEN } from '../common/toastr.service';
 
 @Component({
   templateUrl: 'app/user/profile.component.html',
@@ -21,7 +22,8 @@ export class ProfileComponent implements OnInit{
 
   constructor(
     private authService: AuthService,
-    private router: Router) { }
+    private router: Router,
+    @Inject(TOASTR_TOKEN) private toastr: Toastr) { }
 
   ngOnInit() {
     this.firstName = new FormControl
@@ -39,7 +41,7 @@ export class ProfileComponent implements OnInit{
   saveProfile(formValues) {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['events']);
+     this.toastr.success('Profile Saved')
     }
   }
 
