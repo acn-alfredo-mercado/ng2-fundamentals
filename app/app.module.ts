184442ffd87632cbd026284f3743cddad6a57aca
Mercado, Alfredo I. R <alfredo.i.r.mercado@accenture.com>
2018-02-07 14:44:46 +0800

    Added a duration pipe

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,7 +14,8 @@ import {
     EventRouteActivator,
     EventListResolver,
     CreateSessionComponent,
-    SessionListComponent
+    SessionListComponent,
+    DurationPipe
 } from './events/index';
 
 import { EventsAppComponent } from './events-app.component';
@@ -41,7 +42,8 @@ import { CollapsibleWellComponent } from './common/collapsible-well.component';
         CreateSessionComponent,
         SessionListComponent,
         NavBarComponent,
-        CollapsibleWellComponent
+        CollapsibleWellComponent,
+        DurationPipe
     ],
     bootstrap: [EventsAppComponent],
     providers: [
